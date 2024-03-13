57500ba75a67d3ebef4504bcdbb9e57d71c95f69
Tanya Unterberger <tanya.unterberger@rmit.edu.au>
2024-03-13 14:28:50 +1100

    added exercise 5 files

---
+++ week5/js/js05.js
@@ -0,0 +1,5 @@
+let planets = ['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn', 'Mercury', 'Uranus', 'Neptune', 'Pluto'];
+
+for (let index = 0; index < planets.length; index++) {
+    console.log(planets[index]);
+}
