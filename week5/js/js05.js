2cd4177c5b81339447d5b9a6b02a7b5a50dc6b49
Tanya Unterberger <tanya.unterberger@rmit.edu.au>
2024-03-19 10:02:05 +1100

    Exercise 5

--- week5/js/js05.js
+++ week5/js/js05.js
@@ -1,5 +1,17 @@
-let planets = ['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn', 'Mercury', 'Uranus', 'Neptune', 'Pluto'];
+const planets = ['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn', 'Mercury', 'Uranus', 'Neptune', 'Pluto'];
 
 for (let index = 0; index < planets.length; index++) {
-    console.log(planets[index]);
+    document.write(planets[index] + '<br>');
+}
+
+const obj = {   // object   // key: value  
+    name: 'John',
+    age: 25,
+    city: 'New York',
+    job: 'Developer',
+    happy: true
+};
+
+for (p in obj) {
+    console.log(p + ': ' + obj[p]);
 }
