082918c27e96e63068ca53440c80dda9eb07f5a9
Matthew R <164464295+s3948206@users.noreply.github.com>
2024-08-28 09:36:05 +1000

    created the details page required for pet details when selected

---
+++ a2/details.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Details</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js"></script>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+
+<body>
+    <?php
+    include_once("includes/header.inc");
+    ?>
+
+    <main>
+
+    </main>
+
+    <?php
+    include_once("includes/footer.inc");
+    ?>
+</body>
+
+</html>
