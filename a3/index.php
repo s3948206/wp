205aed5f0d81b192fa2745fa6ed4d705dffa319b
Matthew R <164464295+s3948206@users.noreply.github.com>
2024-10-29 22:08:48 +1100

    carousel image function

--- a3/index.php
+++ a3/index.php
@@ -3,12 +3,38 @@ include_once("includes/header.inc");
 ?>
 
 <main class="home-page">
-    <h1>Pets Victoria</h1>
-    <img src="images/main.jpg" alt="Main Image" class="main-image">
-    <br> <br> <br>
-    <h2>WELCOME TO PET<br>ADOPTION</h2>
-    <br> <br> <br>
+    <div id="petCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/dog1.jpeg" class="d-block" alt="Dog 1" style="width: 100%; height: 300px; object-fit: cover;">
+            </div>
+            <div class="carousel-item">
+                <img src="images/dog2.jpeg" class="d-block" alt="Dog 2" style="width: 100%; height: 300px; object-fit: cover;">
+            </div>
+            <div class="carousel-item">
+                <img src="images/cat1.jpeg" class="d-block" alt="Cat 1" style="width: 100%; height: 300px; object-fit: cover;">
+            </div>
+            <div class="carousel-item">
+                <img src="images/cat2.jpeg" class="d-block" alt="Cat 2" style="width: 100%; height: 300px; object-fit: cover;">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#petCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#petCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <div>
+        <h1>Pets Victoria</h1>
+        <h2>WELCOME TO PET<br>ADOPTION</h2>
+    </div>
 </main>
+
+
 
 <?php
 include_once("includes/footer.inc");
